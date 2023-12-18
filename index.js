740eff94f3e8bde5070169eeea79ab3df464d04f
theonlyJoseph <yousof.noorbakhsh@gmail.com>
2023-12-18 21:48:17 +0330

    Add js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const content = document.querySelectorAll(".card__content");
 const icon = document.querySelectorAll("#reveal-icon");
 
-content.forEach((item, index) => {
-  const header = item.querySelector(".card__question");
-  header.addEventListener("click", () => {
-    item.classList.toggle("open");
+const openContent = (item, header) => {
+  item.classList.toggle("open");
 
-    const answer = item.querySelector(".card__answer");
-    const icon = header.querySelector("#reveal-icon");
-    if (item.classList.contains("open")) {
-      answer.style.height = `${answer.scrollHeight}px`;
+  const answer = item.querySelector(".card__answer");
+  const icon = header.querySelector("#reveal-icon");
+  if (item.classList.contains("open")) {
+    answer.style.height = `${answer.scrollHeight}px`;
 
-      icon.setAttribute("src", "./assets/images/icon-minus.svg");
-    } else {
-      answer.style.height = `0px`;
-      icon.setAttribute("src", "./assets/images/icon-plus.svg");
-    }
-
-    closeContent(index);
-  });
-});
+    icon.setAttribute("src", "./assets/images/icon-minus.svg");
+  } else {
+    answer.style.height = `0px`;
+    icon.setAttribute("src", "./assets/images/icon-plus.svg");
+  }
+};
 
 const closeContent = (index1) => {
   content.forEach((newItem, newIndex) => {
@@ -33,3 +28,23 @@ const closeContent = (index1) => {
     }
   });
 };
+
+content.forEach((item, index) => {
+  const header = item.querySelector(".card__question");
+  header.addEventListener("click", (e) => {
+    // set event target to header
+    const curentItem = e.target.closest(".card__question");
+
+    openContent(item, header, curentItem);
+    closeContent(index);
+    // Expand and close focused element on 'Enter' key press
+    window.addEventListener("keypress", (e) => {
+      let eventKey = e.key;
+
+      if (eventKey === "Enter") {
+        openContent(item, curentItem);
+        closeContent(index);
+      }
+    });
+  });
+});
