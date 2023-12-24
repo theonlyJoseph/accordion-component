b314f183d5b4ca391f3b1f4fef69889db35b5a83
theonlyJoseph <yousof.noorbakhsh@gmail.com>
2023-12-24 17:55:35 +0330

    Update css

--- index.js
+++ index.js
@@ -1,50 +1,28 @@
-const content = document.querySelectorAll(".card__content");
-const icon = document.querySelectorAll("#reveal-icon");
+const header = document.querySelectorAll("[data-accordion-header]");
 
-const openContent = (item, header) => {
-  item.classList.toggle("open");
-
-  const answer = item.querySelector(".card__answer");
-  const icon = header.querySelector("#reveal-icon");
-  if (item.classList.contains("open")) {
-    answer.style.height = `${answer.scrollHeight}px`;
-
+header.forEach((item) => {
+  let answer = item.parentElement.nextElementSibling;
+  item.addEventListener("click", (e) => {
+    let expanded = item.getAttribute("aria-expanded") === "true" || false;
+    let icon = item.parentElement.querySelector("#reveal-icon");
     icon.setAttribute("src", "./assets/images/icon-minus.svg");
-  } else {
-    answer.style.height = `0px`;
-    icon.setAttribute("src", "./assets/images/icon-plus.svg");
-  }
-};
-
-const closeContent = (index1) => {
-  content.forEach((newItem, newIndex) => {
-    if (index1 !== newIndex) {
-      newItem.classList.remove("open");
-
-      const answer = newItem.querySelector(".card__answer");
-      const icon = newItem.querySelector("#reveal-icon");
-      answer.style.height = `0px`;
+    answer.hidden = expanded;
+    item.setAttribute("aria-expanded", !expanded);
+    if (expanded) {
       icon.setAttribute("src", "./assets/images/icon-plus.svg");
     }
   });
-};
-
-content.forEach((item, index) => {
-  const header = item.querySelector(".card__question");
-  header.addEventListener("click", (e) => {
-    // set event target to header
-    const curentItem = e.target.closest(".card__question");
-
-    openContent(item, header, curentItem);
-    closeContent(index);
-    // Expand and close focused element on 'Enter' key press
-    window.addEventListener("keypress", (e) => {
-      let eventKey = e.key;
-
-      if (eventKey === "Enter") {
-        openContent(item, curentItem);
-        closeContent(index);
-      }
-    });
-  });
 });
+
+// const closeContent = (index1) => {
+//   content.forEach((newItem, newIndex) => {
+//     if (index1 !== newIndex) {
+//       newItem.classList.remove("open");
+
+//       const answer = newItem.querySelector(".card__answer");
+//       const icon = newItem.querySelector("#reveal-icon");
+//       answer.style.height = `0px`;
+//       icon.setAttribute("src", "./assets/images/icon-plus.svg");
+//     }
+//   });
+// };
